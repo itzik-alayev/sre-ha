3d1d776d1e81e458799a8a36ec22d4c333b830cb
Itzik Elayev <itzik.elayev@develeap.com>
2021-08-24 22:56:27 +0300

    Added trigger to push everytime since we dont have an indication if it changed or not

--- deployment/docker.tf
+++ deployment/docker.tf
@@ -7,6 +7,10 @@ resource "docker_image" "lambda_image" {
 }
 
 resource "null_resource" "push_image" {
+  triggers = {
+    uuid = uuid()
+  }
+  
   provisioner "local-exec" {
     command = <<-EOT
       aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${split("/", local.ecr_repository_url)[0]}
