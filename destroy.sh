55daaaca704331fcdf31de2c4e96e6f22797b25b
Itzik Elayev <itzik.elayev@develeap.com>
2021-08-24 21:15:05 +0300

    Script now exists in case of an error + got rid of code duplication

--- destroy.sh
+++ destroy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo '-> Destroying "deploy" terraform'
-pushd deploy
-terraform destroy -auto-approve
-popd
+set -e
 
-echo '-> Destroying "infra" terraform'
-pushd infra
-terraform destroy -auto-approve
-popd
+DIRS=(deploy infra)
+
+for dir in $DIRS;
+do
+    echo "-> Destroying '$dir' terraform"
+    pushd $dir
+    terraform destroy -auto-approve
+    popd
+done
 
 echo '-> Environment destroyed.'
