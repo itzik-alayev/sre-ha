4bf1ff85953181f02ea65aa33136ba817abbc80b
Itzik Elayev <itzik.elayev@develeap.com>
2021-08-25 22:36:19 +0300

    Fixed wrong logic of if statement when running script without parameters

--- destroy.sh
+++ destroy.sh
@@ -4,11 +4,13 @@ set -e
 
 DIRS=("deployment" "infra")
 
-if ! [ -z "$1" ] && [ -d "$PWD/$1" ]; then
-    DIRS=("$1")
-else
-    echo "Specified Terraform deployment folder not found ($1), exiting..."
-    exit 1
+if ! [ -z "$1" ]; then
+    if [ -d "$PWD/$1" ]; then
+        DIRS=("$1")
+    else
+        echo "Specified Terraform deployment folder not found ($1), exiting..."
+        exit 1
+    fi
 fi
 
 for dir in ${DIRS[@]};
