17d7f7f626e445d0f6ac7ec7b710a67bba28e04a
Itzik Elayev <itzik.elayev@develeap.com>
2021-08-25 14:16:49 +0300

    Removed unused variable (lambda_image)

--- deployment/variables.tf
+++ deployment/variables.tf
@@ -3,9 +3,3 @@ variable "region" {
   default     = "eu-central-1"
   description = "Desired AWS Region to deploy environment on"
 }
-
-variable "lambda_image" {
-  type        = string
-  default     = "006262944085.dkr.ecr.eu-central-1.amazonaws.com/sre-ha-lambda:latest"
-  description = "AWS ECR Repository URL to be used for AWS Lambda function"
-}
