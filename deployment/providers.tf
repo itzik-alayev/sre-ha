e31eca905eea027225372d31f6a2bc425abf1f3f
itzik-elayev <startukk@gmail.com>
2021-08-29 12:06:40 +0300

    Removed 'docker' provider as it is not used anywhere anymore

--- deployment/providers.tf
+++ deployment/providers.tf
@@ -4,16 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.15.0"
-    }
   }
 }
 
 provider "aws" {
   region = var.region
 }
-
-provider "docker" {}
